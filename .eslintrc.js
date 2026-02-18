06ad13d9bc082e6d42d62eb85114486a5ffa9b8d
Selene <jessre27@gmail.com>
2026-02-18 11:53:12 -0600

    Chore: install project dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: "airbnb-base",
+  rules: {
+    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+    "no-console": "off",
+  },
+};
